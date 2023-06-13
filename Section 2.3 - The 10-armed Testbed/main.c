0d53f54ec46c886ef40451b922eb17ac53fb900c
Jacob Hall <jachallbound@gmail.com>
2023-06-12 21:35:45 -0400

    implemented equ (2.3), adjusted variable names

--- Section 2.3 - The 10-armed Testbed/main.c
+++ Section 2.3 - The 10-armed Testbed/main.c
@@ -10,7 +10,7 @@
 #define K 10
 #define STEPS 1000
 #define RUNS 2000
-#define EPS 0.0 /* Exploratory value */
+#define EPS 0.01 /* Exploratory value */
 
 
 int main (void) {
@@ -19,11 +19,10 @@ int main (void) {
   /* Variables */
   double q[K]; /* q*(a) - true value function */
   double Q[K]; /* Q(a) - estimate value function */
-  double Qn[K]; /* estimate value function numerator */
   double R; /* Reward value */
   double Rr[STEPS]; for(size_t i = 0; i < STEPS; i++) Rr[i] = 0;/* Average reward across runs */
-  size_t At = 0; /* Action to take each step */
-  size_t Ai[K]; /* Numbers of times each action taken */
+  size_t A = 0; /* Action to take each step */
+  size_t n[K]; /* Numbers of times each action taken */
   size_t exploitations = 0; /* Number times exploited */
   size_t explorations = 0; /* Number times explored */
 
@@ -35,30 +34,29 @@ int main (void) {
     /* Set other arrays to zero */
     for (size_t i = 0; i < K; i++) {
       Q[i] = 0.0;
-      Qn[i] = 0.0;
-      Ai[i] = 0;
+      n[i] = 0;
     }
-    At = 0;
+    A = 0;
 
     /* Training steps */
     for (size_t t = 0; t < STEPS; t++) {
       /* First step (t = 0), always explore. Then start calculating value function when t > 0 */
       if (t > 0) {
         /* Generate reward of previous action */
-        randn(q[At], 1, &R, 1);
+        randn(q[A], 1, &R, 1);
         /* Estimate value function */
-        Qn[At] += R; /* Q(a) numerator, sum of rewards for this action */
-        Q[At] = Qn[At]/(double)(++Ai[At]); /* ++Ai[a] tracks sum of times action a was taken */
+        /* 2.4 - Incremental Implementation, reduced memory computation */
+        Q[A] = Q[A] + (R - Q[A])/(double)(++n[A]); /* Equ (2.3) */
       }
 
       /* Choose whether to exploit or explore */
       if (t != 0 && rand() > EPS*(double)RAND_MAX) {
         /* Exploit */
-        At = argmax(Q, K);
+        A = argmax(Q, K);
         exploitations++;
       } else {
         /* Explore */
-        At = rand()%K;
+        A = rand()%K;
         explorations++;
       }
       Rr[t] += R; /* Sum reward over all runs */
