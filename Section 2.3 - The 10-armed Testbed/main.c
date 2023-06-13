ce731f4c381144d71eb47224418b969b22f5fdc2
Jacob Hall <jachallbound@gmail.com>
2023-06-12 21:09:54 -0400

    further simplifications

--- Section 2.3 - The 10-armed Testbed/main.c
+++ Section 2.3 - The 10-armed Testbed/main.c
@@ -10,7 +10,7 @@
 #define K 10
 #define STEPS 1000
 #define RUNS 2000
-#define EPS 0.01 /* Exploratory value */
+#define EPS 0.0 /* Exploratory value */
 
 
 int main (void) {
@@ -21,7 +21,6 @@ int main (void) {
   double Q[K]; /* Q(a) - estimate value function */
   double Qn[K]; /* estimate value function numerator */
   double R; /* Reward value */
-  double Rt[STEPS]; /* Reward based on chosen action at each time step */
   double Rr[STEPS]; for(size_t i = 0; i < STEPS; i++) Rr[i] = 0;/* Average reward across runs */
   size_t At = 0; /* Action to take each step */
   size_t Ai[K]; /* Numbers of times each action taken */
@@ -43,7 +42,7 @@ int main (void) {
 
     /* Training steps */
     for (size_t t = 0; t < STEPS; t++) {
-      /* First step (t = 0), always explore. Then start calculating value function */
+      /* First step (t = 0), always explore. Then start calculating value function when t > 0 */
       if (t > 0) {
         /* Generate reward of previous action */
         randn(q[At], 1, &R, 1);
@@ -51,6 +50,8 @@ int main (void) {
         Qn[At] += R; /* Q(a) numerator, sum of rewards for this action */
         Q[At] = Qn[At]/(double)(++Ai[At]); /* ++Ai[a] tracks sum of times action a was taken */
       }
+
+      /* Choose whether to exploit or explore */
       if (t != 0 && rand() > EPS*(double)RAND_MAX) {
         /* Exploit */
         At = argmax(Q, K);
@@ -60,8 +61,7 @@ int main (void) {
         At = rand()%K;
         explorations++;
       }
-      Rt[t] = R; /* Reward for this run */
-      Rr[t] += Rt[t]; /* Sum reward over all runs */
+      Rr[t] += R; /* Sum reward over all runs */
     }
   }
 
