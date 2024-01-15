ea41c745ba3e121ad403d35eedcac40d3795323c
Jacob Hall <jachallbound@gmail.com>
2024-01-15 18:57:42 -0500

    made volk functions unreadable c:

--- Section 5.3 - Example 5.3/volk_arrays.c
+++ Section 5.3 - Example 5.3/volk_arrays.c
@@ -7,45 +7,103 @@
 /* 2nd dimension length: S2 */
 /* 3rd dimension length: A0 */
 void set_volk_3d(double* array, double value, int i0, int i1, int i2, int len) {
-  // printf("i0 = %2d, i1 = %2d, i2 = %2d | \n", i0, i1, i2);
-  int index_3d = i0*S1*S2 + i1*S2 + i2;
-  if (index_3d < len) array[index_3d] = value;
-  else {
+  #if VOLK_DEBUG
+  printf("i0 = %2d, i1 = %2d, i2 = %2d | \n", i0, i1, i2);
+  #endif /* VOLK_DEBUG */
+
+  int index_3d = i0*S1*S2 + i1*S2 + i2; /* first line of real code */
+
+  #if VOLK_CHECK_LENGTH
+  if (index_3d < len) {
+  #endif /* VOLK_CHECK_LENGTH */
+
+    array[index_3d] = value; /* second line of real code */
+
+  #if VOLK_CHECK_LENGTH
+  } else {
     fprintf(stderr, "(3d) Attempted write beyond array length: %d > %d\n", index_3d, len);
+    
+    #if VOLK_EXIT_IF_OVER_LENGTH
     exit(1);
+    #endif /* VOLK_EXIT_IF_OVER_LENGTH */
+
   }
+  #endif /* VOLK_CHECK_LENGTH */
   return;
 }
 
 void set_volk_4d(double* array, double value, int i0, int i1, int i2, int i3, int len) {
-  // printf("i0 = %2d, i1 = %2d, i2 = %2d i3 = %2d | \n", i0, i1, i2, i3);
-  int index_4d = i0*S1*S2*A0 + i1*S2*A0 + i2*A0 + i3;
-  if (index_4d < len) array[index_4d] = value;
-  else {
+  #if VOLK_DEBUG
+  printf("i0 = %2d, i1 = %2d, i2 = %2d i3 = %2d | \n", i0, i1, i2, i3);
+  #endif /* VOLK_DEBUG */
+
+  int index_4d = i0*S1*S2*A0 + i1*S2*A0 + i2*A0 + i3; /* first line of real code */
+
+  #if VOLK_CHECK_LENGTH
+  if (index_4d < len) {
+  #endif /* VOLK_CHECK_LENGTH */
+
+    array[index_4d] = value; /* second line of real code */
+
+  #if VOLK_CHECK_LENGTH
+  } else {
     fprintf(stderr, "(4d) Attempted write beyond array length: %d > %d\n", index_4d, len);
+    
+    #if VOLK_EXIT_IF_OVER_LENGTH
     exit(1);
+    #endif /* VOLK_EXIT_IF_OVER_LENGTH */
+
   }
+  #endif /* VOLK_CHECK_LENGTH */
   return;
 }
 
 double get_volk_3d(double* array, int i0, int i1, int i2, int len) {
-  // printf("i0 = %2d, i1 = %2d, i2 = %2d | \n", i0, i1, i2);
-  int index_3d = i0*S1*S2 + i1*S2 + i2;
-  if (index_3d < len) return array[index_3d];
-  else {
-    fprintf(stderr, "(3d) Attempted read beyond array length: %d > %d\n", index_3d, len);
+  #if VOLK_DEBUG
+  printf("i0 = %2d, i1 = %2d, i2 = %2d | \n", i0, i1, i2);
+  #endif /* VOLK_DEBUG */
+
+  int index_3d = i0*S1*S2 + i1*S2 + i2; /* first line of real code */
+
+  #if VOLK_CHECK_LENGTH
+  if (index_3d < len) {
+  #endif /* VOLK_CHECK_LENGTH */
+
+    return array[index_3d]; /* second line of real code */
+
+  #if VOLK_CHECK_LENGTH
+  } else {
+    fprintf(stderr, "(3d) Attempted write beyond array length: %d > %d\n", index_3d, len);
+    
+    #if VOLK_EXIT_IF_OVER_LENGTH
     exit(1);
+    #endif /* VOLK_EXIT_IF_OVER_LENGTH */
+    
   }
-  return 0;
+  #endif /* VOLK_CHECK_LENGTH */
 }
 
 double get_volk_4d(double* array, int i0, int i1, int i2, int i3, int len) {
-  // printf("i0 = %2d, i1 = %2d, i2 = %2d i3 = %2d | \n", i0, i1, i2, i3);
-  int index_4d = i0*S1*S2*A0 + i1*S2*A0 + i2*A0 + i3;
-  if (index_4d < len) return array[index_4d];
-  else {
-    fprintf(stderr, "(4d) Attempted read beyond array length: %d > %d\n", index_4d, len);
+  #if VOLK_DEBUG
+  printf("i0 = %2d, i1 = %2d, i2 = %2d i3 = %2d | \n", i0, i1, i2, i3);
+  #endif /* VOLK_DEBUG */
+
+  int index_4d = i0*S1*S2*A0 + i1*S2*A0 + i2*A0 + i3; /* first line of real code */
+
+  #if VOLK_CHECK_LENGTH
+  if (index_4d < len) {
+  #endif /* VOLK_CHECK_LENGTH */
+
+    return array[index_4d]; /* second line of real code */
+
+  #if VOLK_CHECK_LENGTH
+  } else {
+    fprintf(stderr, "(4d) Attempted write beyond array length: %d > %d\n", index_4d, len);
+    
+    #if VOLK_EXIT_IF_OVER_LENGTH
     exit(1);
+    #endif /* VOLK_EXIT_IF_OVER_LENGTH */
+
   }
-  return 0;
+  #endif /* VOLK_CHECK_LENGTH */
 }
--- Section 5.3 - Example 5.3/volk_arrays.h
+++ Section 5.3 - Example 5.3/volk_arrays.h
@@ -12,11 +12,16 @@
 #include "monte_carlo_blackjack.h"
 
 /* Macros */
-/* State space dimensions */
-#define S0 10 /* 3D matrix dimension lengths */
+/* Hard-code multidimensional matrix dimensions */
+#define S0 10 /* matrix dimension lengths */
 #define S1 10
 #define S2 2
 #define A0 2 /* possible actions: 0: hit, 1: stick */
+
+/* Volk flags */
+#define VOLK_DEBUG 0
+#define VOLK_CHECK_LENGTH 1
+#define VOLK_EXIT_IF_OVER_LENGTH 1
 
 /* volk array functions */
 void set_volk_3d(double* array, double value, int i0, int i1, int i2, int len);
